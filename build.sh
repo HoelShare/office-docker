01b9a1c9a999361b77295dc714b60eaa64ba9b2c
Sebastian Hölscher <sebastian@hoelshare.de>
2021-01-03 13:14:32 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,44 @@
-git submodule update --remote --checkout
+tag=$1
 
-docker build -t hoelshare/office-core:latest core
-docker push hoelshare/office-core:latest
+if [ -z "$tag" ]
+then
+  git submodule update --init --remote --checkout
+  git add core/src
+  git add ui/src
+  git commit -m "Create version latest"
+  tag="latest"
+else
+  cd core/src
+  git fetch
+  git checkout $tag
+  if [ $? -ne 0 ]
+  then
+    echo "Version does not exist in core/src" >&2
+    exit 1
+  fi
+  cd ../..
+  cd ui/src
+  git fetch
+  git checkout $tag
+  if [ $? -ne 0 ]
+  then
+    echo "Version does not exist in ui/src" >&2
+    exit 1
+  fi
+  cd ../..
 
-docker build -t hoelshare/office-ui:latest ui
-docker push hoelshare/office-ui:latest
+  git add core/src
+  git add ui/src
+  git commit -m "Create version $tag"
+  git tag $tag
+  git push --tags
+fi
+
+echo "Create Version $tag"
+git submodule update --init --remote --checkout
+
+docker build -t hoelshare/office-core:$tag core
+docker push hoelshare/office-core:$tag
+
+docker build -t hoelshare/office-ui:$tag ui
+docker push hoelshare/office-ui:$tag
